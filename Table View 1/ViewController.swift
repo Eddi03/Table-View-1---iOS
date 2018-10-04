21dbe37c291d17d27d469e24436d15cf5d60d620
Eddi Raimondi <Eddi@Mac-di-Valerio.local>
2018-10-04 18:26:46 +0200

    Initial Commit

---
+++ Table View 1/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Table View 1
+//
+//  Created by Eddi Raimondi on 04/10/2018.
+//  Copyright © 2018 Eddi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+}
