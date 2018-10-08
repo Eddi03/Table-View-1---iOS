71e24a3a9630154d1625ba42ab84bf79c754df4f
Eddi Raimondi <Eddi@Mac-di-Valerio.local>
2018-10-08 09:30:09 +0200

    First Commit

--- Table View 1/ViewController.swift
+++ Table View 1/ViewController.swift
@@ -16,14 +16,18 @@ class ViewController: UIViewController {
     }
 }
 
+// Estensione delle funzioni di Table View
 extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    
+    // Crea 5 celle dentro la tabella
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
     
+    // Scrive "Test" per ogni cella creata precedentemente
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "Test"
-        return cell
+        let cell = UITableViewCell()    // costante cell di UITableViewCell
+        cell.textLabel?.text = "Test"   // scrive "Test" in ogni cella
+        return cell                     // ritorna la cella
     }
 }
